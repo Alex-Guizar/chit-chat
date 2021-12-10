dbd81780399c109b8d15fbc5a1e4940453b27950
Alex Guizar <alexguizar90@gmail.com>
2021-12-10 11:15:38 -0800

    Cleaned up unused imports

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
+// Packages
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 // import the screens
 import Start from './components/Start';
